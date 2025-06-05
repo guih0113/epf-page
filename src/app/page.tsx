4340f969f571ca83c11d642a1789437990a2eb23
guih0113 <gui.henriquess13@gmail.com>
2025-06-05 00:25:34 -0300

    build section main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import SectionMain from "@/components/section-main/page";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/Bg3.png')] bg-no-repeat bg-cover bg-center min-h-screen">
-      <div className="py-24 px-48">
+    <div className="bg-[url('/Bg3.png')] bg-no-repeat bg-cover bg-center min-h-screen w-full">
+      <div className="py-24 px-56">
         <Header />
         <SectionMain />
       </div>
--- src/components/section-main/page.tsx
+++ src/components/section-main/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 export default function SectionMain() {
     return (
-        <div>
-            
-        </div>
+        <section className="w-full flex justify-center items-center">
+            <div className="flex flex-col lg:flex-row items-center lg:items-start justify-evenly w-full max-w-screen-xl mx-auto p-4">
+                <div className="min-w-[400px] w-full lg:w-1/2 lg:ml-[-110px] text-center">
+                    <Image
+                        src="/smartphone.png"
+                        width={700}
+                        height={700}
+                        alt="Smartphone"
+                        quality={100}
+                    />
+                </div>
+
+                <div className="w-full lg:w-1/2 text-center lg:text-right lg:mt-36">
+                    <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl mb-4">
+                        Construa seu futuro <br /> financeiro com <br /> conhecimento e <br /> planejamento
+                    </h1>
+                    <p className="text-[#00DD15] text-lg mb-6">
+                        Criamos um método para você se divertir e aprender um <br /> pouco mais sobre.
+                    </p>
+                    <button className="bg-[#00DD15] text-black py-3 px-6 rounded-full text-lg">
+                        Começar
+                    </button>
+                </div>
+            </div>
+        </section>
     );
 }
